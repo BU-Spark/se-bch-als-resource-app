7e7a0f5dca6ea3964cf5a3882acc4399e675d363
Chris Harris <chris@incisio.com>
2024-04-09 12:03:16 -0400

    removing redundany

--- code/src/pages/index.tsx
+++ code/src/pages/index.tsx
@@ -2,11 +2,6 @@ import QuestionaireBodyContent from './QuestionairePage'
 
 export default function Home() {
   return (
-    // UNCOMMENT FOR TESTING
-    // <div data-testid="questionaire-page">
-    //   <QuestionairePage />
-    // </div>
-    // <HomePage/>
     <QuestionaireBodyContent />
   )
 }
