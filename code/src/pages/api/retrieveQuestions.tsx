31fa8b44d545c26cb1b2c51245632b8d244e8c8a
Chris Harris <chris@incisio.com>
2024-04-09 15:50:02 -0400

    updating api to transform to IQuestionList interface

--- code/src/pages/api/retrieveQuestions.tsx
+++ code/src/pages/api/retrieveQuestions.tsx
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { COMMUNICATION_FORM_ID, TYPEFORM_API_URL } from '../../constants/globals';
+import {
+    IChoice, ILogic, IQuestionList, IQuestion, ISolution, IBodyContent
+} from '../../types/api_types';
 
 export default async function retrieveQuestions(req: NextApiRequest, res: NextApiResponse) {
     const { flowName } = req.query; 
@@ -9,8 +12,31 @@ export default async function retrieveQuestions(req: NextApiRequest, res: NextAp
         try {
             const response = await fetch(`${TYPEFORM_API_URL}/forms/${COMMUNICATION_FORM_ID}`);
             const data = await response.json();
-            const { fields, logic } = data; // Extracting fields and logic arrays from data object
-            res.status(200).json({ fields, logic }); // Returning only fields and logic arrays
+            
+            // Transform fields to IQuestion[]
+            const questions: IQuestion[] = data.fields.map((field: any) => ({
+                id: field.id,
+                title: field.title,
+                ref: field.ref,
+                type: field.type,
+                choices: field.properties.choices?.map((choice: any) => ({
+                    id: choice.id,
+                    ref: choice.ref,
+                    label: choice.label,
+                    link: choice.link || null
+                })),
+                description: field.properties.description
+            }));
+
+            
+            const logic: ILogic[] = data.logic;
+
+            const questionList: IQuestionList = {
+                questions: questions,
+                logic: logic
+            };
+
+            res.status(200).json(questionList);
         } catch (error) {
             res.status(500).json({ error: 'Internal Server Error' });
         }
