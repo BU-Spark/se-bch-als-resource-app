48c530142a979f9e2bca5f81122e095f7eac4f6f
Marek Pinto <marek.pinto@gmail.com>
2024-04-14 22:31:55 -0400

    Implemented Hamburger Menu

--- code/src/components/Navbar/Nav.tsx
+++ code/src/components/Navbar/Nav.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useState} from 'react';
 import { createStyles, Header, Container, Group, Burger, rem} from '@mantine/core';
 import Image from 'next/image'
 import Link from 'next/link';
@@ -22,6 +23,13 @@ const useStyles = createStyles(() => ({
 
 
 const Nav = () => {
+
+  const [opened, setOpened] = useState(false);
+
+  const toggleMenu = () => {
+    setOpened(!opened);
+  };
+
   const { classes } = useStyles();
   // Renders a Header component with a height of HEADER_HEIGHT, borderBottom set to 0, borderTop set to 4, and withBorder enabled Wrapped in a Container component with the inner class from useStyles and fluid property
     return (
@@ -38,7 +46,48 @@ const Nav = () => {
         />
         </Link>
         </Group>
-        <Burger size="lg" color="#254885" opened={false} />
+        <Burger size="lg" color="#254885" opened={opened} onClick={toggleMenu} />
+        {opened && (
+        <div style={{
+          position: 'absolute',
+          top: '50px', // Adjust based on the height of the Burger
+          left: '0',
+          width: '100%', // Adjust the width as needed
+          boxShadow: '0 2px 5px rgba(0,0,0,0.2)',
+          zIndex: 1000,
+          backgroundColor: '#fff',
+        }}>
+          <ul style={{
+            listStyleType: 'none',
+            margin: '0',
+            padding: '0',
+          }}>
+            <li style={{
+              padding: '10px 20px',
+              borderBottom: '1px solid #ddd',
+            }}>
+              <Link href="/" style={{
+                  textDecoration: 'none',
+                  color: '#254885',
+                  display: 'block',
+                }} onClick={toggleMenu}>
+                Home
+              </Link>
+            </li>
+            <li style={{
+              padding: '10px 20px',
+            }}>
+              <Link href="/bookmarks" style={{
+                  textDecoration: 'none',
+                  color: '#254885',
+                  display: 'block',
+                }} onClick={toggleMenu}>
+                Bookmarks
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
       </Container>
       </Header>
     )
