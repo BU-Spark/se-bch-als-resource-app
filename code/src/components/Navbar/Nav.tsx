4c4e3570a9346bda6d7d13bb21aeec373bf8da77
zhr114 <zhr114@bu.edu>
2024-12-10 18:32:10 -0500

    Nav

--- code/src/components/Navbar/Nav.tsx
+++ code/src/components/Navbar/Nav.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import { Popover, Slider, Switch, Group, Text, Button } from "@mantine/core";
 import {
